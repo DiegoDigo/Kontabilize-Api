2cf8b07d9e07acb1ed950c1f95cc379f123c7173
Diego Domingos <di3g0d0ming05@gmail.com>
2020-09-26 10:23:58 -0300

    FEAT: criação de usuario de teste.

--- Kontabilize.Infra/Context/KontabilizeContext.cs
+++ Kontabilize.Infra/Context/KontabilizeContext.cs
@@ -1,6 +1,10 @@
+using System.Reflection;
 using Kontabilize.Domain.CompanyContext.Entities;
 using Kontabilize.Domain.UserContext.Entities;
+using Kontabilize.Domain.UserContext.Entities.Enums;
+using Kontabilize.Infra.Extensions;
 using Kontabilize.Infra.Mappers;
+using Kontabilize.Shared.VOs;
 using Microsoft.EntityFrameworkCore;
 
 namespace Kontabilize.Infra.Context
@@ -10,23 +14,25 @@ namespace Kontabilize.Infra.Context
         public DbSet<User> Users { get; set; }
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<Address> Addresses { get; set; }
-        
+
         public DbSet<Company> Companies { get; set; }
-        
-        
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql(@"Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=kontabilize;");
+            optionsBuilder.UseNpgsql(
+                @"Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=kontabilize;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new UserMap());
-            modelBuilder.ApplyConfiguration(new ProfileMap());
-            modelBuilder.ApplyConfiguration(new AddressMap());
-            modelBuilder.ApplyConfiguration(new CompanyMap());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
+            modelBuilder.Seed();
+            
             base.OnModelCreating(modelBuilder);
         }
+
+        
     }
 }
---
+++ Kontabilize.Infra/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Kontabilize.Domain.UserContext.Entities;
+using Kontabilize.Domain.UserContext.Entities.Enums;
+using Kontabilize.Shared.VOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kontabilize.Infra.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+
+            Users().ToList().ForEach(user =>
+            {
+                modelBuilder.Entity<User>(userEntity =>
+                {
+                    userEntity.HasData(MapUserToAnonymousObject(user));
+                    userEntity.OwnsOne(x => x.Email).HasData(MapEmailToAnonymousObject(user));
+                });
+            });
+        }
+
+        private static object MapUserToAnonymousObject(User user)
+        {
+            return new
+            {
+                user.Id,
+                user.Active,
+                user.Password,
+                user.Role,
+                user.CreateAt
+            };
+        }
+        
+        private static object MapEmailToAnonymousObject(User user)
+        {
+            return new
+            {
+                UserId = user.Id,
+                user.Email.Address,
+            };
+        }
+
+        private static IEnumerable<User> Users() 
+        {
+            return new List<User>
+            {
+                InitialUserAdmin(),
+                InitialUserAccountant(),
+                InitialUserCustomer()
+            };
+        }
+        
+        private static User InitialUserAdmin()
+        {
+            var email = new Email("admin@gmail.com");
+            var user = new User(email, ERole.Admin);
+            user.HasPassword("12345678");
+            return user;
+        }
+        
+        private static User InitialUserAccountant()
+        {
+            var email = new Email("contador@gmail.com");
+            var user = new User(email, ERole.Accountant);
+            user.HasPassword("12345678");
+            return user;
+        }
+        
+        private static User InitialUserCustomer()
+        {
+            var email = new Email("usuario@gmail.com");
+            var user = new User(email, ERole.Customer);
+            user.HasPassword("12345678");
+            return user;
+        }
+    }
+}
